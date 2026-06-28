fe9ebe47e30f6e524cc7ff6caddfff3fbd53de11
Varsanyi14 <varsanyi@icloud.com>
2026-06-28 14:57:40 -0400

    thailand_country_update

--- src/data/index.js
+++ src/data/index.js
@@ -1,13 +1,13 @@
 import vietnam from './vietnam.js';
 import japan from './japan.js';
 import mexico from './mexico.js';
+import thailand from './thailand.js';
 
 const soon = [
-  { slug: 'thailand', name: 'Thailand', live: false },
   { slug: 'italy', name: 'Italy', live: false },
   { slug: 'france', name: 'France', live: false },
   { slug: 'spain', name: 'Spain', live: false },
   { slug: 'united-kingdom', name: 'United Kingdom', live: false },
 ];
 
-export const countries = [vietnam, japan, mexico, ...soon];
+export const countries = [vietnam, japan, mexico, thailand, ...soon];
